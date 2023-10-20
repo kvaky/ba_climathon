3d57026a63271eca442e25e49eaa009d49e4714f
Peter Kvackay <gmail@kvaky.com>
2023-10-20 23:24:21 +0200

    Add api to return geojson

--- api/app/app.py
+++ api/app/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import geopandas as gpd
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+gdf = gpd.read_file("data/Kriticke_zony_mock/Kriticke_zony.shp").rename(
+    columns={"Index": "sensitivity_index"}
+)
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+@app.route("/")
+def hello():
+    return gdf.to_json()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
